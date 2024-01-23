46cf6297d89d6a2c5ed5a2299a1710028304d6e8
Israr <bhatisrarshamim@gmail.com>
2024-01-23 13:44:19 +0530

    yes I an committing

--- app/src/main/java/com/example/brandnewchat/ChatApplication.kt
+++ app/src/main/java/com/example/brandnewchat/ChatApplication.kt
@@ -1,20 +1,19 @@
 package com.example.brandnewchat
 
 import android.app.Application
-import com.example.brandnewchat.data.appContainer.AppContainer
 import com.example.brandnewchat.data.appContainer.AppDataContainer
 
 class ChatApplication :Application() {
-       lateinit var container: AppContainer
-//    val container by lazy {
-//        AppDataContainer(this)
-//    }
+//       lateinit var container: AppContainer
+    val container by lazy {
+        AppDataContainer(this)
+    }
 //            init {
 //    container = AppDataContainer(this)
 //}
 
     override fun onCreate() {
         super.onCreate()
-        container = AppDataContainer(this)
+//        container = AppDataContainer(this)
     }
 }
